8f0b0950ddfe289881734a21b62c790dbf466b9a
Akin Gokce <akinaskingokce@gmail.com>
2023-11-13 16:08:02 +0300

    iter

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
+const { SerialPort } = require('serialport')
+const commander = require('./lib/commander.js');
+const parser = require('./lib/parser.js');
+const util = require('./lib/util.js');
+
+
+
+let mainWindow = "";
+let serialPort = ""
 
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -12,32 +21,101 @@ function createWindow () {
     }
   })
 
-  // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
+  startSeriPort();
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+ 
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+/**************SERI PORT ********************/
+
+function startSeriPort() {
+
+  serialPort = new SerialPort({
+    path: 'COM1',
+    baudRate: 115200,
+    autoOpen: false,
+  })
+
+  serialPort.open(function (err) {
+    if (err) {
+      return console.log('Error opening port: ', err.message)
+    }
+  })
+
+    // The open event is always emitted
+  serialPort.on('open', function() {
+    // open logic
+    console.log("Port open oldu");  
+    //serialPort.write("*A2s4*I0:2*I1:3*I2:0*Y0:73*Z")    
+    generateCommand("A","","0");
+
+  })
+
+  serialPort.on('readable', function () {
+    console.log('Data readable:', serialPort.read())
+  })
+
+  // Switches the port into "flowing mode"
+  serialPort.on('data', function (data) {
+    console.log('Data:', data.toString())
+  })
+
+}
+
+/********************SERI PORT END *****************/
+
+async function generateCommand(Action,Parameters,ServiceIndex) {
+  try {
+
+      let parameters = "";
+      const action = Action;
+      const serviceIndex = ServiceIndex;
+
+      if(Parameters === "")
+      { 
+        console.log("Buradai");
+        parameters = "";
+      }
+      else{
+        parameters = {Parameters}
+        console.log("Buradaiss");
+
+      }
+
+      const creatingMessage = await commander.createMessage(action,parameters,serviceIndex);
+      console.log("creatingMessage", creatingMessage);
+
+      const updatedMessage = await commander.createCommand(creatingMessage);
+      console.log("createCommand:", updatedMessage);
+
+      serialPort.write(updatedMessage) ;
+      console.log("Mesajın son hali:", updatedMessage);
+  } catch (error) {
+      console.error(error);
+  }
+}
+
+
+
+
+
+
+
+
+
